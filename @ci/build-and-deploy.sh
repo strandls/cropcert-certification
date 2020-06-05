0eeea06477a5dd9c324f7edd59aad97c1f97ed21
Harsh Zalavadiya <harshzalavadiya@users.noreply.github.com>
2020-06-05 05:56:48 +0530

    :recycle: updated hiberante path

--- @ci/build-and-deploy.sh
+++ @ci/build-and-deploy.sh
@@ -3,11 +3,11 @@
 # Minimal script that updates default credintials with prod
 
 echo "⚙️ Updating configuration"
-sed 's|>postgres<|>'$DB_USER'<|g' -i WebContent/WEB-INF/classes/hibernate.cfg.xml
-sed 's|>postgres123<|>'$DB_PASSWORD'<|g' -i WebContent/WEB-INF/classes/hibernate.cfg.xml
-sed 's|localhost|'$DB_HOST'|g' -i WebContent/WEB-INF/classes/hibernate.cfg.xml
-sed 's|5432|'$DB_PORT'|g' -i WebContent/WEB-INF/classes/hibernate.cfg.xml
-sed 's|cropcert|'$DB_NAME'|g' -i WebContent/WEB-INF/classes/hibernate.cfg.xml
+sed 's|>postgres<|>'$DB_USER'<|g' -i src/main/webapp/WEB-INF/classes/hibernate.cfg.xml
+sed 's|>postgres123<|>'$DB_PASSWORD'<|g' -i src/main/webapp/WEB-INF/classes/hibernate.cfg.xml
+sed 's|localhost|'$DB_HOST'|g' -i src/main/webapp/WEB-INF/classes/hibernate.cfg.xml
+sed 's|5432|'$DB_PORT'|g' -i src/main/webapp/WEB-INF/classes/hibernate.cfg.xml
+sed 's|cropcert|'$DB_NAME'|g' -i src/main/webapp/WEB-INF/classes/hibernate.cfg.xml
 
 echo "📦 Building package"
 /opt/apache-maven/bin/mvn clean package
